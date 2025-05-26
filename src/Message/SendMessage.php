1c51464da9ed4a7208076c2b82234abf70bbe950
kule85 <kulence@gmail.com>
2025-05-26 14:15:27 +0200

    update send message and handler

--- src/Message/SendMessage.php
+++ src/Message/SendMessage.php
@@ -1,13 +1,24 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Message;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * This class is used both as a message (for the message bus) and as a DTO.
+ * Because of that, we decided to add validation constraints directly to the properties,
+ * so the message can be validated during instantiation or processing.
+ */
 class SendMessage
 {
-    public function __construct(
-        public string $text,
-    )
+    #[Assert\NotBlank(message: 'Text is required')]
+    #[Assert\Type('string', message: 'Text must be a string')]
+    public string $text;
+
+    public function __construct(string $text)
     {
+        $this->text = $text;
     }
 }
